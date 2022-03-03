03888915bd0be6e567749d6def20db0282797f55
WyroneBlue <wyroneblue@gmail.com>
2022-03-03 15:36:16 +0100

    added error routes

---
+++ routes/error.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    const page = {
+      title: "Not Found"
+    };
+    res.status(404).render('404.hbs', { page: page })
+})
+
+
+module.exports = router;
