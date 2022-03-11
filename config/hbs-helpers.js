3b890bdeb743a0495685c6b40a2b76503a3e36b7
WyroneBlue <wyroneblue@gmail.com>
2022-03-11 02:09:45 +0100

    added new custom hbs helpers

--- config/hbs-helpers.js
+++ config/hbs-helpers.js
@@ -14,9 +14,24 @@ const bool = (check, val) => {
 	return check === true ? val : ''; 
 }
 
+const stars = (rating, stars) => {
+    const width = 5 * 35;
+    return multiply(divide(rating, stars), width);
+}
+
+// https://stackoverflow.com/questions/11924452/iterating-over-basic-for-loop-using-handlebars-js
+const times = (n, block) => {
+    var accum = '';
+    for(var i = 0; i < n; ++i)
+        accum += block.fn(i);
+    return accum;
+};
+
 module.exports = {
     multiply: multiply,
     add: add,
     divide: divide,
     bool: bool,
+    stars: stars,
+    times: times,
 }
