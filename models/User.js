7e0d968b801a22e9d2839a14737df8ddea729b97
WyroneBlue <wyroneblue@gmail.com>
2022-03-12 23:19:01 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -7,9 +7,9 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   name: String,
   email: String,
-  school: String,
-  img: String,
-  reviews: [{ type: Schema.Types.ObjectId, ref: 'Rating' }]
+  img: {type: String, default: 'https://picsum.photos/200'},
+  reviews: [{ type: Schema.Types.ObjectId, ref: 'Review' }],
+  created: {type: Date, default: Date.now},
 });
 
 const User = mongoose.model('User', UserSchema);
